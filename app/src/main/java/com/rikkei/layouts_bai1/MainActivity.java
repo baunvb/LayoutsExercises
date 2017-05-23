060328a512bd280839767c10c0ad3523ba2a5c5c
baunvb <baunvb@gmail.com>
2017-05-23 14:08:37 +0700

    Initial commit

---
+++ app/src/main/java/com/rikkei/layouts_bai1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rikkei.layouts_bai1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+    private RadioButton mRBtnColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai1);
+    }
+
+}
